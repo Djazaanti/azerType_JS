7aa90777a2acdac54b64a25440ab5d4979ef6ad3
Djazaanti MSA <dmsa@soldis.local>
2026-06-12 11:59:21 +0200

    dev : correction function jouer OK - L'appli s'arrête après avoir saisi les propositions faites sans afficher le message de fin avec score.

---
+++ src/js/const.js
@@ -0,0 +1,7 @@
+// Cette variable contient la liste des mots et des phrases proposée au joueur
+const listePropositions = [
+    ["Bonjour", "cachalot", "Pétunia", "Serviette"],
+    ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"]
+]
+// cette variable contient le nombre de tentaives autorisées pour lancer le jeu
+const nombreTentativesAutorisees = 3
